15657cab2308fdb239eaa0f353e1e53d182c0c2e
sestaton <statonse@gmail.com>
2014-10-06 16:10:16 -0700

    reorganize project; add scripts and method to install deps

---
+++ install_deps.sh
@@ -0,0 +1,21 @@
+cd src
+
+## Velvet
+curl -L https://api.github.com/repos/dzerbino/velvet/tarball > velvet.tar.gz
+tar xzf velvet.tar.gz
+mv dzerbino-velvet* velvet && cd velvet
+make
+mv velvetg ../../bin
+mv velveth ../../bin
+cd ..
+
+## VelvetOptimiser
+curl -L https://api.github.com/repos/sestaton/VelvetOptimiser/tarball > VO.tar.gz
+tar xzf VO.tar.gz
+mv sestaton-VelvetOptimiser* VelvetOptimiser
+cd ..
+
+## Pairfq-lite
+cd scripts
+wget https://raw.githubusercontent.com/sestaton/Pairfq/master/scripts/pairfq_lite.pl
+
