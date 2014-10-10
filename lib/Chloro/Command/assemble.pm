62d160df232c8fef355e6623410f81c2594f261e
sestaton <statonse@gmail.com>
2014-10-10 15:54:17 -0700

    start adding method for assembly

--- lib/Chloro/Command/assemble.pm
+++ lib/Chloro/Command/assemble.pm
@@ -6,12 +6,15 @@ use strict;
 use warnings;
 use Chloro -command;
 use IPC::System::Simple qw(system);
-use LWP::UserAgent;
 use File::Basename;
+use Try::Tiny;
 
 sub opt_spec {
     return (    
-	[ "outfile|o=s",  "A file to place the Pfam2GO mappings" ],
+	[ "infile|i=s", "A file of screened chlorplast sequences to be assembled" ],
+	[ "threads|t=i", "The number of threads (hash steps) to execute simultaneously (Default: 1)" ],
+	[ "hashs|s=i",  "The starting hash size (Default: 59)" ],
+	[ "hashe|e=i",  "The maximum hash size (Default: 89)" ],
     );
 }
 
@@ -23,7 +26,7 @@ sub validate_args {
 	system([0..5], "perldoc $command");
     }
     else {
-	$self->usage_error("Too few arguments.") unless $opt->{outfile};
+	$self->usage_error("Too few arguments.") unless $opt->{infile};
     }
 } 
 
@@ -31,11 +34,41 @@ sub execute {
     my ($self, $opt, $args) = @_;
 
     exit(0) if $self->app->global_options->{man};
-    my $outfile = $opt->{outfile};
 
-    my $result  = _fetch_mappings($outfile);
+    my $result = _run_assembly($opt);
 }
 
+sub _run_assembly {
+    my ($opt) = @_;
+    my $infile = $opt->{infile};
+    my $hashs  = $opt->{hashs};
+    my $hashe  = $opt->{hashe};
+    my $thread = $opt->{threads};
+
+    my $exit_value;
+    $hashs  //= 59;
+    $hashe  //= 89;
+    $thread //= 1;
+    my ($file, $dir, $ext) = fileparse($infile, qr/\.[^.]*/);
+    my $dirname = "VelvetOpt_k$hashs-k$hashe";
+
+    my @vo_cmd = "src/VelvetOptimiser/VelvetOptimiser.pl ".
+	         "-s $hashs ".
+		 "-e $hashe ".
+		 "-t $thread ".
+		 "-p $dirname ".
+		 "-d $dirname ".
+		 "-f '-fasta -shortPaired $infile'";
+
+    try {
+        $exit_value = system([0..5], @vo_cmd);
+    }
+    catch {
+        die "\nERROR: VelvetOptimiser exited with exit value $exit_value. Here is the exception: $_\n";
+    };
+ 
+		 
+}
 
 
 1;
