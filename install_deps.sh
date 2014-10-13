763016f757df21817320f5436ec9deda65822e36
sestaton <statonse@gmail.com>
2014-10-13 16:46:34 -0700

    change build process; do not modify vo code

--- install_deps.sh
+++ install_deps.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-mkdir src
+mkdir src data
 cd src
 
 ## Velvet
 curl -L https://api.github.com/repos/dzerbino/velvet/tarball > velvet.tar.gz
 tar xzf velvet.tar.gz
 mv dzerbino-velvet* velvet && cd velvet
-make
+make MAXKMERLENGTH=99
 cd ..
 
 ## VelvetOptimiser
 curl -L https://api.github.com/repos/sestaton/VelvetOptimiser/tarball > VO.tar.gz
 tar xzf VO.tar.gz
 mv sestaton-VelvetOptimiser* VelvetOptimiser
-sed -i 's/`velveth/..\/velvet\/velveth/' VelvetOptimiser/VelvetOpt/hwrap.pm
-sed -i 's/`velvetg/..\/velvet\/velvetg/' VelvetOptimiser/VelvetOpt/gwrap.pm
 cd ..
 
 ## Pairfq-lite
 cd scripts
-wget https://raw.githubusercontent.com/sestaton/Pairfq/master/scripts/pairfq_lite.pl
-
+wget --no-check-certificate https://raw.githubusercontent.com/sestaton/Pairfq/master/scripts/pairfq_lite.pl
+chmod +x pairfq_lite.pl
