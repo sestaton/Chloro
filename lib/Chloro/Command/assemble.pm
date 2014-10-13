e4f9beb968bf56a85e482039d4a549800cd3b0b4
sestaton <statonse@gmail.com>
2014-10-13 16:47:00 -0700

    modify paths to output data

--- lib/Chloro/Command/assemble.pm
+++ lib/Chloro/Command/assemble.pm
@@ -6,15 +6,19 @@ use strict;
 use warnings;
 use Chloro -command;
 use IPC::System::Simple qw(system);
+use Capture::Tiny qw(:all);
 use File::Basename;
+use File::Spec;
+use Cwd qw(abs_path);
 use Try::Tiny;
 
 sub opt_spec {
     return (    
-	[ "infile|i=s", "A file of screened chlorplast sequences to be assembled" ],
-	[ "threads|t=i", "The number of threads (hash steps) to execute simultaneously (Default: 1)" ],
-	[ "hashs|s=i",  "The starting hash size (Default: 59)" ],
-	[ "hashe|e=i",  "The maximum hash size (Default: 89)" ],
+	[ "pairfile|p=s",   "A file of paired, interleaved chlorplast sequences to be assembled" ],
+	[ "singletons|s=s", "The file of unpaired, singleton sequences" ], 
+	[ "threads|t=i",    "The number of threads (hash steps) to execute simultaneously (Default: 1)" ],
+	[ "hashs|i=i",      "The starting hash size (Default: 59)" ],
+	[ "hashe|j=i",      "The maximum hash size (Default: 89)" ],
     );
 }
 
@@ -26,7 +30,7 @@ sub validate_args {
 	system([0..5], "perldoc $command");
     }
     else {
-	$self->usage_error("Too few arguments.") unless $opt->{infile};
+	$self->usage_error("Too few arguments.") unless $opt->{pairfile};
     }
 } 
 
@@ -40,34 +44,45 @@ sub execute {
 
 sub _run_assembly {
     my ($opt) = @_;
-    my $infile = $opt->{infile};
-    my $hashs  = $opt->{hashs};
-    my $hashe  = $opt->{hashe};
-    my $thread = $opt->{threads};
+    my $pairfile   = abs_path($opt->{pairfile});
+    my $singletons = abs_path($opt->{singletons}); 
+    my $hashs      = $opt->{hashs};
+    my $hashe      = $opt->{hashe};
+    my $thread     = $opt->{threads};
+
+    my $file = __FILE__;
+    my $cmd_dir = basename(dirname(abs_path($file)));
+    my $hmm_dir = basename(dirname($cmd_dir));
+    my $chl_dir = basename(dirname($hmm_dir));
+    my $vel_dir = File::Spec->catdir($chl_dir, 'src', 'velvet');
+    my $vo      = File::Spec->catfile(abs_path($chl_dir), 'src', 'VelvetOptimiser', 'VelvetOptimiser.pl');
+    local $ENV{PATH} = "$ENV{PATH}:$vel_dir";
 
     my $exit_value;
     $hashs  //= 59;
     $hashe  //= 89;
     $thread //= 1;
-    my ($file, $dir, $ext) = fileparse($infile, qr/\.[^.]*/);
+    my ($ifile, $idir, $iext) = fileparse($pairfile, qr/\.[^.]*/);
     my $dirname = "VelvetOpt_k$hashs-k$hashe";
 
-    my @vo_cmd = "src/VelvetOptimiser/VelvetOptimiser.pl ".
+    my @vo_cmd = "$vo ".
 	         "-s $hashs ".
 		 "-e $hashe ".
 		 "-t $thread ".
 		 "-p $dirname ".
 		 "-d $dirname ".
-		 "-f '-fasta -shortPaired $infile'";
+		 "-f '-fasta -shortPaired $pairfile -fasta -short $singletons'";
 
-    try {
-        $exit_value = system([0..5], @vo_cmd);
-    }
-    catch {
-        die "\nERROR: VelvetOptimiser exited with exit value $exit_value. Here is the exception: $_\n";
-    };
- 
-		 
+    my ($stdout, $stderr, @res) = capture { system([0..5], @vo_cmd); };
+
+    #try {
+    #    $exit_value = capture([0..5], @vo_cmd);
+    #}
+    #catch {
+    #    die "\nERROR: VelvetOptimiser exited with exit value $exit_value. Here is the exception: $_\n";
+    #};
+
+    return $exit_value;
 }
 
 
