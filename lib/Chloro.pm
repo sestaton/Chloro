510b32eb56ce67b266212ff930abc8f6e065716f
sestaton <statonse@gmail.com>
2014-10-28 19:46:06 -0400

    bump version

--- lib/Chloro.pm
+++ lib/Chloro.pm
@@ -8,11 +8,11 @@ Chloro - Automated chloroplast genome assembly
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 sub global_opt_spec {
     [ 'man|m' => "Get the manual entry for a command" ];
